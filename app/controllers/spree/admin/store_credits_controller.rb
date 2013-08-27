4b7f6b4528b18dd6d599cfdc8e48df5044461438
André Teves <andreteves@gmail.com>
2013-08-26 22:16:07 -0300

    added the possibility to edit remaining_amount

--- app/controllers/spree/admin/store_credits_controller.rb
+++ app/controllers/spree/admin/store_credits_controller.rb
@@ -1,7 +1,7 @@
 module Spree
   class Admin::StoreCreditsController < Admin::ResourceController
-    before_filter :check_amounts, :only => [:edit, :update]
-    prepend_before_filter :set_remaining_amount, :only => [:create, :update]
+    #before_filter :check_amounts, :only => [:edit, :update]
+    #prepend_before_filter :set_remaining_amount, :only => [:create, :update]
 
     private
     def check_amounts
