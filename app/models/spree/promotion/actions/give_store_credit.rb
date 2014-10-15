8786d921595c6900638598080c498316ef1b9548
André Teves <andreteves@gmail.com>
2014-10-14 22:46:09 -0300

    small fix

--- app/models/spree/promotion/actions/give_store_credit.rb
+++ app/models/spree/promotion/actions/give_store_credit.rb
@@ -7,7 +7,7 @@ module Spree
     def perform(options = {})
       reason = "#{promotion.name}"
       if user = options[:user]
-        return if user.store_credits.where(:reason => reason).present?
+        return if Spree::StoreCredit.where(category: preferred_category).where(:reason => reason).present?
         user.store_credits.create(:amount => preferred_amount, :remaining_amount => preferred_amount, :reason => reason, :category => preferred_category)
       end
     end
